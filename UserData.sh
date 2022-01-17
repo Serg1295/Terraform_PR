9d4a4895d47e60c9d4548e39520c87a8534208fd
Sergo Hovakimyan <sergo.hovakimyan.12@gmail.com>
2022-01-17 17:11:07 +0400

    initial commit

--- UserData.sh
+++ UserData.sh
@@ -4,5 +4,6 @@ echo "\n" >> /home/ubuntu/.bashrc
 echo "export DB_PASSWORD=\`aws ssm get-parameters --name DB_PASSWORD --region eu-central-1 --with-decryption --output text --query Parameters[].Value\`" >> /home/ubuntu/.bashrc
 echo "export DB_USER=\`aws ssm get-parameters --name DB_USER --region eu-central-1 --output text --query Parameters[].Value\`" >> /home/ubuntu/.bashrc
 echo "export DB_URL=\`aws ssm get-parameters --name DB_URL --region eu-central-1 --output text --query Parameters[].Value\`" >> /home/ubuntu/.bashrc
-su - ubuntu -c "rake db:create:all"
-su - ubuntu -c "rails s -e production -d"
+echo "export RAILS_ENV=production"
+#su - ubuntu -c "rake db:create:all"
+#su - ubuntu -c "rails s -e production -d"
