b9d98d53865f3e0447686fd49cbe71747e62911d
Sergo Hovakimyan <sergo.hovakimyan.12@gmail.com>
2022-01-17 21:11:35 +0400

    initial commit

--- UserData.sh
+++ UserData.sh
@@ -5,6 +5,6 @@ echo "export DB_PASSWORD=\`aws ssm get-parameters --name DB_PASSWORD --region eu
 echo "export DB_USER=\`aws ssm get-parameters --name DB_USER --region eu-central-1 --output text --query Parameters[].Value\`" >> /home/ubuntu/.bashrc
 echo "export DB_URL=\`aws ssm get-parameters --name DB_URL --region eu-central-1 --output text --query Parameters[].Value\`" >> /home/ubuntu/.bashrc
 echo "export RAILS_ENV=production" >> /home/ubuntu/.bashrc
-su - ubuntu -c "rake db:create"
+su - ubuntu -c "cd /var/www/P161App && rake db:create"
 su - ubuntu -c "cd /var/www/P161App && sudo chown -R ubuntu:ubuntu ."
 su - ubuntu -c "cd /var/www/P161App && rails assets:precompile"
